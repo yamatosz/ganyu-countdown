1d09f53f8f7eaa5a018da9dad53a6564a08e6370
yamatosz <lucasj191218@gmail.com>
2022-01-02 23:32:35 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+import Countdown from "./countdown.js";
+
+const tempoParaAGanyu = new Countdown("25 January 2022 23:00:00 GMT-0300");
+const tempos = document.querySelectorAll("[data-time]");
+
+function mostrarTempo() {
+    tempos.forEach((tempo, index) => {
+        tempo.innerHTML = tempoParaAGanyu.total[index]
+    });
+}
+mostrarTempo();
+setInterval(mostrarTempo, 1000);
